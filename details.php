3b3f810c97f90dadd75eaa2a2ea21ef08a5b7572
Sachin9599 <sachin832002@gmail.com>
2022-09-15 16:46:42 +0530

    job search

---
+++ details.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Online Job portal</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- <h3 class="my-2 mx-2">ONLINE JOB PORTAL</h3> -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse mx-4" id="basicExampleNav">
+
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="welcome.php">Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Top Companies</a>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true">My Admisstration</a>
+                    <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">My bookmarks</a>
+                        <a class="dropdown-item" href="#">My job</a>
+                        <a class="dropdown-item" href="#">Profile</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="landing">
+        <img src="img/search.jpg" alt="search" width="100%">
+    </div>
+    <section id="about-area" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title  text-center">
+                        <h1>Jobs Details</h1>
+                        <span class="title-line"><i class="fa fa-car"></i></span>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <section class="row" id="about">
+                <div class="col-lg-6">
+                    <div class="display-table">
+                        <div class="display-table-cell">
+                            <div class="about-content">
+                                <h2>XYZ POSITION</h2>
+                                <p class="about_data">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+                                    totam, asperiores exercitationem, molestias quidem veritatis nisi, iste doloremque
+                                    sequi ad esse consequatur ullam quos dolor voluptate. Voluptas culpa fugit quasi
+                                    debitis cumque. Repellendus illo reiciendis magnam consequatur maxime alias possimus
+                                    doloribus officiis voluptate ipsa. Incidunt officia quam tempore sunt placeat hic,
+                                    quas sequi, quis distinctio aliquid dolorem, eligendi aliquam omnis.
+                                    <br>
+                                    <br>
+                                </p>
+                                <div>
+                                    <table class="mx-2">
+                                        <tr>
+                                            <th>Minimum Education</th>
+                                            <td>B.tech, BCA , MCA, M.Tech</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Location</th>
+                                            <td>Gurgaon,Hydrabad,Delhi</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Contact</th>
+                                            <td>+910000000000</td>
+
+                                        </tr>
+                                        <tr>
+                                            <th>Email</th>
+                                            <td>jackson@gmail.com</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Job Post</th>
+                                            <td>All Positions</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Set Skills</th>
+                                            <td>Good knowledege of computer</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="col-lg-6 my-4">
+                    <div class="about-image ">
+                        <img src="img/main.jpg" alt="ACR" width="86%">
+                    </div>
+                </div>
+            </section>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+            <footer class="text-center">
+    <h4>Resesrved Under All &copy;  2022</h4>
+</footer>
+
+
+
+
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+                crossorigin="anonymous"></script>
+            <script src="index.js"></script>
+</body>
+
+</html>
---
+++ partials/navbar.php
@@ -0,0 +1,45 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse mx-4" id="basicExampleNav">
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="welcome.php">Home
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Top Companies</a>
+      </li>
+
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true">My Admisstration</a>
+        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">My bookmarks</a>
+          <a class="dropdown-item" href="#">My job</a>
+          <a class="dropdown-item" href="#">Profile</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">logout</a>
+      </li>
+    </ul>
+  </div>
+  <div class="d-flex align-items-center">
+          <button type="button" class="btn btn-primary me-3" onclick="window.location.href = 'login.php';">
+            Login</button>
+            <button type="button" class="btn btn-primary me-3" onclick="window.location.href = 'signup.php';">
+            Sign Up</button>
+        </div>
+</nav>
